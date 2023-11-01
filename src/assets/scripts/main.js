275d9b718e2d3c8a6d939668467c6492bc361724
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2023-11-01 19:53:36 +0100

    image gallery half done

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,34 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
-/**
- * Write any other JavaScript below
- */
+const initSlider = () => {
+  const imageList = document.querySelector(".slider-wrapper .image-list");
+  const slideButtons = document.querySelectorAll(
+    ".slider-wrapper .slide-button"
+  );
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+  // La acción slide de las imágenes según los clicks del botón slide.
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = imageList.clientWidth * direction;
+
+      imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+  });
+
+  //Mostrar  esconder el botón de slide según la posición.
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display =
+      imageList.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display =
+      imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+
+  imageList.addEventListener("scroll", () => {
+    handleSlideButtons();
+  });
+};
+
+window.addEventListener("load", initSlider);
